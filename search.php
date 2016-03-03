5eb7a52939632e23505c917153f09077fa0e2184
kotrma <shparavalo78nenad@yahoo.com>
2016-03-03 16:26:11 +0100

    small changes

---
+++ search.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search Results</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no"/>
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/search.css"/>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+
+    <!--[if lt IE 9]>
+    <html class="lt-ie9">
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/..">
+            <img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820"
+                 alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+        </a>
+    </div>
+    <script src="js/html5shiv.js"></script>    
+  	<script src='js/selectivizr-min.js'></script>
+    <![endif]-->
+
+    <script src='js/device.min.js'></script>
+</head>
+
+<body>
+<div class="page">
+    <!--========================================================
+                              HEADER
+    =========================================================-->
+     <header>
+        <div id="stuck_container" class="stuck_container">
+          <div class="container">
+            <div class="brand">
+              <h1 class="brand_name">
+                <a href="./">Office Rent</a>
+              </h1>
+            </div>
+
+            <nav class="nav">
+              <ul class="sf-menu" data-type="navbar">
+                <li class="active">
+                  <a href="./">Home</a>
+                </li>
+                <li>
+                  <a href="index-1.html">About Us</a>
+                  <ul>
+                    <li>
+                      <a href="#">Lorem ipsum dolor</a>
+                    </li>
+                    <li>
+                      <a href="#">Ait amet conse</a>
+                    </li>
+                    <li>
+                      <a href="#" class="sub-menu">Ctetur adipisicing elit</a>
+                      <ul>
+                        <li>
+                          <a href="#">Latest</a>
+                        </li>
+                        <li>
+                          <a href="#">Archive</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <a href="#">Sed do eiusmod</a>
+                    </li>
+                    <li>
+                      <a href="#">Tempor incididunt</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="index-2.html">Our listings</a>
+                </li>
+                <li>
+                  <a href="index-3.html">Requests</a>
+                </li>
+                <li>
+                  <a href="index-4.html">Contact Us</a>
+                </li>
+              </ul>
+            </nav>
+
+            <div class="contact-info">
+              <dl class="text-right">
+                <dt>Call our helpline:</dt>
+                <dd><a href="callto:#">800-1234-5678</a></dd>
+              </dl>
+            </div>
+          </div>
+        </div>
+      </header>
+
+    <!--========================================================
+                              CONTENT
+    =========================================================-->
+    <main>
+        <section id="content" class="content">
+            <div class="container ">
+                <h3>Search Results</h3>
+                <div id="search-results"></div>
+            </div>
+        </section>
+    </main>
+    <!--========================================================
+                              FOOTER
+    =========================================================-->
+    <footer id="footer" class="footer">
+        <section class="rights">
+              <div class="container">
+                <p>
+                  Office Rent &#169; <span id="copyright-year"></span>.
+                  <a href="index-5.html">Privacy Policy</a>
+                </p>
+              </div>
+            </section>
+    </footer>
+</div>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
